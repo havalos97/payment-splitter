1cfffa18e9cee2f196b398811a8678de74587b87
Hector Avalos <hg.avalosc97@gmail.com>
2024-12-16 23:47:15 -0600

    Updates PWA manifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,10 +38,34 @@ export default defineNuxtConfig({
       short_name: 'Repartir gastos',
       description: 'PWA que facilita el repartir gastos entre amigos',
       lang: 'es',
-      display: 'fullscreen',
+      display: 'standalone',
       background_color: '#ffffff',
       theme_color: '#bda8ff',
       start_url: '/',
+      icons: [
+        {
+          src: '/android-chrome-192x192.png',
+          sizes: '192x192',
+        },
+        {
+          src: '/android-chrome-512x512.png',
+          sizes: '512x512',
+        },
+        {
+          src: '/apple-touch-icon.png',
+        },
+        {
+          src: '/favicon-16x16.png',
+          sizes: '16x16',
+        },
+        {
+          src: '/favicon-32x32.png',
+          sizes: '32x32',
+        },
+        {
+          src: '/favicon.ico',
+        },
+      ],
     },
   },
 })
