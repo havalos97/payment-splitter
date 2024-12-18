03ea55216a9128bc545b1b8c2699099205720f16
Hector Avalos <hg.avalosc97@gmail.com>
2024-12-17 20:24:32 -0600

    One last nuxt config pwa configuration update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
@@ -30,32 +32,56 @@ export default defineNuxtConfig({
     vue: {
       template: {},
     },
+    plugins: [
+      VitePWA({
+        registerType: 'autoUpdate',
+        devOptions: {
+          enabled: true,
+        },
+        manifest: {
+          name: 'Repartir gastos',
+          short_name: 'PaySplit',
+          description: 'App que facilita el repartir gastos entre amigos',
+          lang: 'es',
+          start_url: '/',
+          display: 'standalone', // Ensures the app behaves like a native app
+          background_color: '#ffffff',
+          theme_color: '#42b883',
+          icons: [
+            {
+              src: '/android-chrome-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: '/android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+          ],
+        },
+        workbox: {
+          runtimeCaching: [
+            {
+              urlPattern: ({ request }) =>
+                request.destination === 'document' || request.destination === 'script',
+              handler: 'NetworkFirst',
+            },
+            {
+              urlPattern: ({ request }) => request.destination === 'image',
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'images-cache',
+                expiration: {
+                  maxEntries: 50,
+                  maxAgeSeconds: 30 * 24 * 60 * 60,
+                },
+              },
+            },
+          ],
+        },
+      }),
+    ],
   },
   modules: ['@vite-pwa/nuxt'],
-  pwa: {
-    manifest: {
-      name: 'Repartir gastos',
-      short_name: 'Repartir gastos',
-      description: 'PWA que facilita el repartir gastos entre amigos',
-      lang: 'es',
-      display: 'standalone',
-      background_color: '#ffffff',
-      theme_color: '#ffffff',
-      start_url: '/',
-      icons: [
-        {
-          src: '/public/android-chrome-192x192.png',
-          sizes: '192x192',
-        },
-        {
-          src: '/public/android-chrome-512x512.png',
-          sizes: '512x512',
-        },
-        {
-          src: '/public/apple-touch-icon.png',
-          sizes: '512x512',
-        },
-      ],
-    },
-  },
 })
