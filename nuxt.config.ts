8710aead712ffd551bf11cbd2507c878271d3711
Hector Avalos <hg.avalosc97@gmail.com>
2025-01-13 15:20:41 -0600

    Another pwa test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,12 +43,12 @@ export default defineNuxtConfig({
       theme_color: '#f3f4f6',
       icons: [
         {
-          src: '/icon.png',
+          src: '/android-chrome-192x192.png',
           sizes: '192x192',
           type: 'image/png',
         },
         {
-          src: '/icon-512.png',
+          src: '/android-chrome-512x512.png',
           sizes: '512x512',
           type: 'image/png',
         },
