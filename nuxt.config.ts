c77c4dff9c02a6c8b2561e34ef8105f136c0a339
Hector Avalos <hg.avalosc97@gmail.com>
2025-01-13 15:16:14 -0600

    Updates pwa configuration?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,3 @@
-import { VitePWA } from 'vite-plugin-pwa'
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
@@ -29,73 +27,69 @@ export default defineNuxtConfig({
     },
   },
   modules: ['@vite-pwa/nuxt'],
-  vite: {
-    plugins: [
-      VitePWA({
-        registerType: 'autoUpdate',
-        devOptions: {
-          enabled: true, // Enables service worker in dev mode for testing
+  pwa: {
+    registerType: 'autoUpdate',
+    devOptions: {
+      enabled: true, // Enables service worker in dev mode for testing
+    },
+    manifest: {
+      name: 'Payment Splitter',
+      short_name: 'PaySplit',
+      description: 'A simple app to split payments',
+      lang: 'en',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#f3f4f6',
+      theme_color: '#f3f4f6',
+      icons: [
+        {
+          src: '/icon.png',
+          sizes: '192x192',
+          type: 'image/png',
         },
-        manifest: {
-          name: 'Payment Splitter',
-          short_name: 'PaySplit',
-          description: 'A simple app to split payments',
-          lang: 'en',
-          start_url: '/',
-          display: 'standalone',
-          background_color: '#f3f4f6',
-          theme_color: '#f3f4f6',
-          icons: [
-            {
-              src: '/icon.png',
-              sizes: '192x192',
-              type: 'image/png',
-            },
-            {
-              src: '/icon-512.png',
-              sizes: '512x512',
-              type: 'image/png',
-            },
-          ],
+        {
+          src: '/icon-512.png',
+          sizes: '512x512',
+          type: 'image/png',
         },
-        workbox: {
-          runtimeCaching: [
-            {
-              urlPattern: /\/_nuxt\/.*\.js$/, // Cache JS files
-              handler: 'StaleWhileRevalidate',
-              options: {
-                cacheName: 'nuxt-js-cache',
-                expiration: {
-                  maxEntries: 50,
-                  maxAgeSeconds: 86400, // Cache for 1 day
-                },
-              },
+      ],
+    },
+    workbox: {
+      runtimeCaching: [
+        {
+          urlPattern: /\/_nuxt\/.*\.js$/, // Cache JS files
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'nuxt-js-cache',
+            expiration: {
+              maxEntries: 50,
+              maxAgeSeconds: 86400, // Cache for 1 day
             },
-            {
-              urlPattern: /\/_nuxt\/.*\.css$/, // Cache CSS files
-              handler: 'StaleWhileRevalidate',
-              options: {
-                cacheName: 'nuxt-css-cache',
-                expiration: {
-                  maxEntries: 50,
-                  maxAgeSeconds: 86400, // Cache for 1 day
-                },
-              },
-            },
-            {
-              urlPattern: /\/$/, // Cache the HTML (root route) page
-              handler: 'CacheFirst',
-              options: {
-                cacheName: 'html-cache',
-                expiration: {
-                  maxEntries: 10, // Only keep 1 HTML page in the cache
-                  maxAgeSeconds: 86400, // Cache for 1 day
-                },
-              },
-            },
-          ],
+          },
         },
-      }),
-    ],
+        {
+          urlPattern: /\/_nuxt\/.*\.css$/, // Cache CSS files
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'nuxt-css-cache',
+            expiration: {
+              maxEntries: 50,
+              maxAgeSeconds: 86400, // Cache for 1 day
+            },
+          },
+        },
+        {
+          urlPattern: /\/$/, // Cache the HTML (root route) page
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'html-cache',
+            expiration: {
+              maxEntries: 10, // Only keep 1 HTML page in the cache
+              maxAgeSeconds: 86400, // Cache for 1 day
+            },
+          },
+        },
+      ],
+    },
   },
 })
